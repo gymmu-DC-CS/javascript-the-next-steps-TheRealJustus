0634cc11067d7ae91cd646dfb10907d43e11bed5
Justus <e232305@edu.sbl.ch>
2023-11-28 16:32:00 +0100

    Nothing works

--- helper.js
+++ helper.js
@@ -1,5 +1,5 @@
 import * as utils from "../utils.js"
-import * as funcs from "../script.js"
+import * as funcs from "./script.js"
 ;[funcs, utils].forEach((moduleName) => {
   Object.getOwnPropertyNames(moduleName).forEach((name) => {
     if (typeof moduleName[name] === "function") {
