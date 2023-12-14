980268b15c07fe6a1e00683032fe37c203115115
Justus <e232305@edu.sbl.ch>
2023-12-14 15:33:56 +0100

    Exercise 4-6

--- script.js
+++ script.js
@@ -50,10 +50,55 @@ export function exercise04(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+    const previousElement = input[i - 1]
 
-    if (currentElement === "Word") {
+    if (currentElement === " " && previousElement != "-") {
       count = count + 1
     } else if (currentElement === "0") count = count - 1
   }
-  return count
+  return count + 1
+}
+
+export function exercise05(args) {
+  const input = args
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i].charCodeAt()
+    if (currentElement >= 65 && currentElement <= 90) {
+      return true
+    }
+  }
+
+  return false
+}
+
+export function exercise06(args) {
+  const input = args
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i].charCodeAt()
+    if (
+      (currentElement >= 33 && currentElement <= 47) ||
+      (currentElement >= 58 && currentElement <= 64) ||
+      (currentElement >= 91 && currentElement <= 96) ||
+      (currentElement >= 123 && currentElement <= 126)
+    ) {
+      return true
+    }
+  }
+
+  return false
+}
+
+export function exercise07(args) {
+  const input = args
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "and") {
+      return true
+    }
+  }
+
+  return false
 }
